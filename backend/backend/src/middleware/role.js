7302ccae9563998ed08c490455abbf5e347810e1
ELSAYED <elsayedhamad67@gmail.com>
2026-05-05 02:05:53 +0300

    Delete backend/backend/src directory

--- backend/backend/src/middleware/role.js
+++
@@ -1,9 +0,0 @@
-// middleware/role.js
-module.exports = (role) => {
-  return (req, res, next) => {
-    if (req.user.role !== role)
-      return res.status(403).json({ msg: "Access denied" });
-
-    next();
-  };
-};
