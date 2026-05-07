64cb3ca1b8911561e441b67cbfc545dc300a9d89
ELSAYED <elsayedhamad67@gmail.com>
2026-05-07 19:03:55 +0300

    Fix base URL in API configuration

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://https://food-rescue-isf6.vercel.app//api',
+  baseURL: 'https://food-rescue-isf6.vercel.app/api',
 });
 
 api.interceptors.request.use((config) => {
